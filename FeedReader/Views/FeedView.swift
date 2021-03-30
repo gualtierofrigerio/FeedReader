48a0f113cfd5d6cf01e847ffdee406c5bd03b901
Gualtiero <gualtiero.frigerio@gmail.com>
2021-03-30 10:12:44 +0200

    sheet instead of navigation link

--- FeedReader/Views/FeedView.swift
+++ FeedReader/Views/FeedView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct FeedView: View {
     @ObservedObject var viewModel:FeedViewModel
+    
     var body: some View {
-        VStack {
-            Text("Feed")
+        NavigationView {
             List(viewModel.feed.entries) { entry in
-                FeedEntryView(entry:entry)
-            }
+                Button {
+                    buttonTap(entry: entry)
+                } label: {
+                    FeedEntryView(entry:entry)
+                }
+            }.navigationTitle("Feed")
         }
+        .sheet(isPresented: $showSheet) {
+            dynamicSheet
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Error"),
+                  message: Text("Cannot open article"),
+                  dismissButton: .default(Text("Ok")))
+        }
+    }
+    
+    // MARK: - Private
+    @State private var showAlert = false
+    @State private var showSheet = false
+    
+    @ViewBuilder private var dynamicSheet: some View {
+        if let entry = viewModel.selectedEntry,
+           let url = entry.url {
+            ArticleView(url: url)
+        }
+        else {
+            Text("Error while opening the requested article")
+        }
+    }
+    
+    private func buttonTap(entry:FeedEntry) {
+        if let _ = entry.url {
+            viewModel.selectedEntry = entry
+            showSheet = true
+        }
+        else {
+            showAlert = true
+        }
+    }
+}
+
+struct ConsoleLogView:View {
+    init(_ message:String) {
+        print(message)
+    }
+    
+    var body: some View {
+        EmptyView()
     }
 }
 
