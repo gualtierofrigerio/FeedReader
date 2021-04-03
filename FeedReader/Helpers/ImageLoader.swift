a154cfeeb68ade722bf9c4846c12c5dde0299dc9
Gualtiero <gualtiero.frigerio@gmail.com>
2021-04-03 09:56:56 +0200

    added ImageLoader

---
+++ FeedReader/Helpers/ImageLoader.swift
@@ -0,0 +1,37 @@
+//
+//  ImageLoader.swift
+//  FeedReader
+//
+//  Created by Gualtiero Frigerio on 30/03/21.
+//
+
+import Combine
+import Foundation
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+    
+    func load(url:URL) {
+        loadImage(fromURL: url)
+    }
+    
+    func load(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        loadImage(fromURL: url)
+    }
+    
+    private func loadImage(fromURL url:URL) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
