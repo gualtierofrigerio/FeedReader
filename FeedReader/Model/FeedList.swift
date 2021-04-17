059e6f59c2c6e5d4265ff9eeab8d62f98aaffbd5
Gualtiero <gualtiero.frigerio@gmail.com>
2021-04-17 18:52:38 +0200

    removed FeedListEntry

--- FeedReader/Model/FeedList.swift
+++ FeedReader/Model/FeedList.swift
@@ -7,20 +7,21 @@
 
 import Foundation
 
-enum FeedListEntryType: String {
-    case online
-    case local
-    case aggregated
-}
-
-struct FeedListEntry {
-    var name:String
-    var url:String
-    var type:FeedListEntryType
-}
-
 struct FeedList {
     var entries:[FeedListEntry]
+}
+
+extension FeedList {
+    static func initWithArray(_ array:[[String:String]]) -> Self {
+        let entries = array.compactMap({ element in
+            FeedListEntry.initWithDictionary(element)
+        })
+        return FeedList(entries: entries)
+    }
+    
+    func toArray() -> [[String:String]] {
+        entries.map {$0.toDictionary()}
+    }
 }
 
 extension FeedList {
