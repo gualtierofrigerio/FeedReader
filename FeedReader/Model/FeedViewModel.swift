81c504ee42e68f1a2901417cac42ed55bd2e95cf
Gualtiero <gualtiero.frigerio@gmail.com>
2021-04-24 13:53:14 +0200

    added favorites

--- FeedReader/Model/FeedViewModel.swift
+++ FeedReader/Model/FeedViewModel.swift
@@ -10,11 +10,14 @@ import Foundation
 
 class FeedViewModel:ObservableObject {
     @Published var feed:Feed = Feed(entries: [])
+    @Published var feedName = "Feed"
     // selectedEntry is @Published so once we select an entry
     // the SwiftUI view will reload and have access to showAlert and showSheet
     @Published var selectedArticleViewModel:ArticleViewModel?
-    var showAlert = false
-    var showSheet = false
+    @Published var selectedArticleIsFavorite = false
+    @Published var showAlert = false
+    @Published var showSheet = false
+    
     
     var errorMessage:String = ""
     
@@ -32,30 +35,54 @@ class FeedViewModel:ObservableObject {
                 }
             }
         }
+        self.favoritesHandler = FavoritesHandler.shared
     }
     
     init(withURLString urlString:String) {
         if let url = URL(string: urlString) {
-            let xmlHelper = XMLHelper()
-            let publisher = xmlHelper.parseXML(atURL: url, elementName: "item")
-            cancellable = publisher.sink { xmlArray in
-                if let xmlArray = xmlArray {
-                    self.feed = Feed.createFromArray(xmlArray)
-                }
-            }
+            loadFromURL(url)
         }
     }
     
-    func updateModel() {
-        feedTopicHelper.updateModel(withEntries: feed.entries)
+    init(withEntries entries:[FeedEntry], title:String) {
+        self.feed = Feed(entries: entries)
     }
     
-    func userChangedCategory(category:String, toEntry:FeedEntry) {
-        //feedTopicHelper.updateCategory(category:category, entry:toEntry)
-        feed.updateCategory(category: category, forEntry: toEntry)
+    init(withFeedEntry entry:FeedListEntry) {
+        switch entry.type {
+        case .online:
+            if let url = URL(string: entry.url) {
+                loadFromURL(url)
+            }
+        case .favorites:
+            feed = Feed(entries: favoritesHandler.getFavoritesFeedEntries())
+            type = .favorites
+        }
+        feedName = entry.name
+    }
+    
+    func closeSheet() {
+        showSheet = false
+    }
+    
+    func entryIsFavorite(_ entry:FeedEntry) -> Bool {
+        favoritesHandler.isArticleFavorite(article: entry)
+    }
+    
+    func entryToggleFavorite(_ entry:FeedEntry) {
+        favoritesHandler.toggleFavorite(entry:entry)
+        refreshFeed()
+    }
+    
+    func selectedArticleToggleFavorite() {
+        guard let selectedArticleViewModel = selectedArticleViewModel else { return }
+        let article = selectedArticleViewModel.article
+        entryToggleFavorite(article)
+        selectedArticleIsFavorite = favoritesHandler.isArticleFavorite(article: article)
     }
     
     func userSelectedEntry(_ entry:FeedEntry) {
+        selectedArticleIsFavorite = favoritesHandler.isArticleFavorite(article: entry)
         selectedArticleViewModel = ArticleViewModel(withArticle: entry)
         if let _ = entry.url {
             showSheet = true
@@ -68,5 +95,26 @@ class FeedViewModel:ObservableObject {
     
     // MARK: - Private
     private var cancellable:AnyCancellable?
+    private var favoritesHandler:FavoritesHandler = FavoritesHandler.shared
     private var feedTopicHelper = FeedTopicHelper()
+    private var type:FeedListEntryType = .online
+    
+    private func loadFromURL(_ url:URL) {
+        let xmlHelper = XMLHelper()
+        let publisher = xmlHelper.parseXML(atURL: url, elementName: "item")
+        cancellable = publisher.sink { xmlArray in
+            if let xmlArray = xmlArray {
+                self.feed = Feed.createFromArray(xmlArray)
+            }
+        }
+    }
+    
+    private func refreshFeed() {
+        if type == .favorites {
+            feed = Feed(entries: favoritesHandler.getFavoritesFeedEntries())
+        }
+        else {
+            objectWillChange.send()
+        }
+    }
 }
