dc8c57167df2b722bc298fd08ad02f0363c36f91
Gualtiero <gualtiero.frigerio@gmail.com>
2021-04-24 13:52:02 +0200

    removed updateCategory

--- FeedReader/Model/Feed.swift
+++ FeedReader/Model/Feed.swift
@@ -9,17 +9,6 @@ import Foundation
 
 struct Feed {
     var entries:[FeedEntry]
-    
-    mutating func updateCategory(category:String, forEntry entry:FeedEntry) {
-        for i in 0..<entries.count {
-            if entries[i].title == entry.title {
-                var newEntry = entries[i]
-                newEntry.selectedCategory = category
-                entries[i] = newEntry
-                break
-            }
-        }
-    }
 }
 
 extension Feed {
