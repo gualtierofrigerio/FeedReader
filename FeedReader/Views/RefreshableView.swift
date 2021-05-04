87981cff18a75c37cb6ee37985c7aa12ec51565c
Gualtiero <gualtiero.frigerio@gmail.com>
2021-05-04 11:17:08 +0200

    added RefreshableView

---
+++ FeedReader/Views/RefreshableView.swift
@@ -0,0 +1,48 @@
+//
+//  RefreshableView.swift
+//  FeedReader
+//
+//  Created by Gualtiero Frigerio on 04/05/21.
+//
+
+import SwiftUI
+
+struct OffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+struct RefreshableView<Content:View>: View {
+    
+    
+    init(action: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.refreshAction = action
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                content()
+                    .anchorPreference(key: OffsetPreferenceKey.self, value: .top) {
+                        geometry[$0].y
+                    }
+            }
+            .onPreferenceChange(OffsetPreferenceKey.self) { offset in
+                refreshAction()
+            }
+        }
+    }
+    
+    private var content: () -> Content
+    private var refreshAction: () -> Void
+}
+
+struct RefreshableView_Previews: PreviewProvider {
+    static var previews: some View {
+        RefreshableView(action: {}, content:{Text("test")})
+    }
+}
