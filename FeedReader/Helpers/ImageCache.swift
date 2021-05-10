12448057c9995fbb93b0600f520d6bb282de22a4
Gualtiero <gualtiero.frigerio@gmail.com>
2021-05-10 16:01:40 +0200

    directly call RESTClient

--- FeedReader/Helpers/ImageCache.swift
+++ FeedReader/Helpers/ImageCache.swift
@@ -8,24 +8,60 @@
 import Foundation
 import UIKit
 
+typealias ImageCacheCompletion = (UIImage?) -> Void
+
 class ImageCache {
     static let shared = ImageCache()
     
-    func imageForURL(_ url:String) -> UIImage? {
-        cache.first { record in
-            record.url == url
-        }?.image
+    func imageForURL(_ url:URL, completion: @escaping ImageCacheCompletion) {
+        if let cachedImage = cache.first(where: { record in
+            record.urlString == url.absoluteString
+        }) {
+            completion(cachedImage.image)
+        }
+        else {
+            loadImage(url: url, completion: completion)
+        }
     }
     
-    func setImage(_ image:UIImage, forUrl url:String) {
-        cache.append(ImageCacheRecord(url: url, image: image))
-    }
-    
+    // MARK: - Private
     private var cache:[ImageCacheRecord] = []
+    private let cacheSize = 10
+    
+    private func imageFromData(_ data:Data) -> UIImage? {
+        UIImage(data: data)
+    }
+    
+    private func loadImage(url:URL, completion: @escaping ImageCacheCompletion) {
+        RESTClient.loadData(atURL: url) { result in
+            switch result {
+            case .success(let data):
+                if let image = self.imageFromData(data) {
+                    completion(image)
+                    self.setImage(image, forUrl: url.absoluteString)
+                }
+                else {
+                    completion(nil)
+                }
+            case .failure(_ ):
+                completion(nil)
+            }
+        }
+    }
+    
+    private func setImage(_ image:UIImage, forUrl url:String) {
+        if cache.count >= cacheSize {
+            cache.remove(at: 0)
+            
+        }
+        cache.append(ImageCacheRecord(image: image, urlString: url))
+    }
+    
+    // MARK: - ImageCacheRecord
     
     struct ImageCacheRecord {
-        var url:String
         var image:UIImage
+        var urlString:String
     }
 }
 
