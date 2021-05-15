46bab4b66af162081e2fd7c24f1d614d4ea94124
Gualtiero <gualtiero.frigerio@gmail.com>
2021-05-15 16:37:46 +0200

    changed enum FeedListEntryType

--- FeedReader/Model/FeedListEntry.swift
+++ FeedReader/Model/FeedListEntry.swift
@@ -7,9 +7,38 @@
 
 import Foundation
 
-enum FeedListEntryType: String {
-    case online = "online"
-    case favorites = "favorites"
+indirect enum FeedListEntryType: Equatable {
+    case online
+    case favorites
+    case aggregated([FeedListEntryType])
+}
+
+extension FeedListEntryType: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .online:
+            return "online"
+        case .favorites:
+            return "favorites"
+        case .aggregated:
+            return "aggregated"
+        }
+    }
+}
+
+extension FeedListEntryType {
+    static func fromString(_ string:String) -> Self? {
+        switch string {
+        case "online":
+            return .online
+        case "favorites":
+            return .favorites
+        case "aggregated":
+            return .aggregated([])
+        default:
+            return nil
+        }
+    }
 }
 
 struct FeedListEntry {
@@ -25,13 +54,13 @@ extension FeedListEntry {
               let type = dictionary["type"] else { return nil }
         
         var entryType = FeedListEntryType.online
-        if let typeFromString = FeedListEntryType(rawValue: type) {
+        if let typeFromString = FeedListEntryType.fromString(type) {
             entryType = typeFromString
         }
         return FeedListEntry(name: name, url: url, type: entryType)
     }
     
     func toDictionary() -> [String:String] {
-        ["name" : name, "url" : url, "type" : type.rawValue]
+        ["name" : name, "url" : url, "type" : type.description]
     }
 }
