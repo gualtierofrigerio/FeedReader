3dcdb865a0ebd6534e1c4bffdba47ded4a7e66b6
Gualtiero <gualtiero.frigerio@gmail.com>
2021-05-15 16:36:29 +0200

    added AddNewFeed

---
+++ FeedReader/Views/AddNewFeed.swift
@@ -0,0 +1,83 @@
+//
+//  AddNewFeed.swift
+//  FeedReader
+//
+//  Created by Gualtiero Frigerio on 14/05/21.
+//
+
+import SwiftUI
+
+struct AddNewFeed: View {
+    @ObservedObject var viewModel:SelectFeedViewModel
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Add a new Feed")
+                    .font(Font.headline)
+                    .padding()
+                Form {
+                    TextField("Name", text: $newFeedName)
+                    TextField("URL", text: $newFeedURL)
+                }
+                Button {
+                    addEntry()
+                } label: {
+                    Text("Add")
+                }
+                .disabled(newFeedURL.isEmpty || newFeedURL.isEmpty)
+            }.frame(height: 250)
+            Divider()
+                .padding()
+            Text("Create an aggregated feed")
+                .font(Font.headline)
+                .padding()
+            Form {
+                List {
+                    ForEach(viewModel.selectedFeedEntries, id: \.name) { entry in
+                        Button {
+                            viewModel.toggleSelectedEntry(name: entry.name)
+                        } label: {
+                            HStack {
+                                Text(entry.name)
+                                Spacer()
+                                if entry.selected {
+                                    Image(systemName: "checkmark.square")
+                                }
+                                else {
+                                    Image(systemName: "square")
+                                }
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+            Button {
+                print("confirm")
+            } label: {
+                Text("Add aggregated")
+            }.disabled(viewModel.aggregatedButtonDisabled)
+        }
+    }
+    
+    // MARK: - Private
+    @State private var newFeedName = ""
+    @State private var newFeedURL = ""
+    
+    private func addEntry() {
+        viewModel.addEntry(name: newFeedName, url: newFeedURL)
+        clearNewFeedEntries()
+    }
+    
+    private func clearNewFeedEntries() {
+        newFeedName = ""
+        newFeedURL = ""
+    }
+}
+
+struct AddNewFeed_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewFeed(viewModel: SelectFeedViewModel())
+    }
+}
