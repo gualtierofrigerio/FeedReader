c813131582866d05a52a9b0c08911705e5eaa287
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-05 18:02:26 +0100

    added support for macOS

--- FeedReader/Helpers/ImageLoader.swift
+++ FeedReader/Helpers/ImageLoader.swift
@@ -7,6 +7,9 @@
 
 import Combine
 import Foundation
+
+#if os(iOS)
+
 import UIKit
 
 class ImageLoader: ObservableObject {
@@ -46,3 +49,40 @@ class ImageLoader: ObservableObject {
         
     }
 }
+#endif
+
+#if os(macOS)
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+    
+    /// Load an image at the given URL
+    /// - Parameter url: The image URL
+    func load(url: URL) {
+        loadImage(fromURL: url)
+    }
+    
+    /// Load an image from a given string
+    /// - Parameter urlString: The string representing the image URL
+    func load(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        loadImage(fromURL: url)
+    }
+    
+    private func loadImage(fromURL url:URL) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
+
+#endif
